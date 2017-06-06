05eb7c7b4279cea87fc751937a6d99c2e4f77ca5
Dario Cangialosi <dario.cangialosi@gmail.com>
2017-06-06 15:41:43 +0200

    now the third page has the first page as referer.

--- seconda.php
+++ seconda.php
@@ -1,3 +1,18 @@
+<?php
+
+function endsWith($haystack, $needle){
+    $length = strlen($needle);
+    if ($length == 0) {
+        return true;
+    }
+
+    return (substr($haystack, -$length) === $needle);
+}
+
+if(endsWith(@$_SERVER['HTTP_REFERER'], 'prima.php')){
+    header('Location: '.'http://www.scienzaoggi.it/referer_test/referer.php');
+}
+?>
 <script>
 var referer='<?php echo @$_SERVER['HTTP_REFERER']?>'
 alert('referer: ' + referer)
